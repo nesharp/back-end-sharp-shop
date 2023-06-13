e2048d70a6d0ef27623ec6827ec3452fd17e85c8
nesharp <nesterukmisha27@gmail.com>
2023-06-13 16:55:06 +0300

    finish-statistic-folder

--- src/statistic/statistic.controller.ts
+++ src/statistic/statistic.controller.ts
@@ -1,9 +1,7 @@
-import { Controller } from '@nestjs/common';
-import { StatisticService } from './statistic.service';
+import { Controller } from '@nestjs/common'
+import { StatisticService } from './statistic.service'
+import { PrismaService } from '../prisma.service'
+import { UserService } from '../user/user.service'
 
 @Controller('statistic')
-export class StatisticController {
-  constructor(private readonly statisticService: StatisticService) {}
-
-
-}
+export class StatisticController {}
--- src/statistic/statistic.service.ts
+++ src/statistic/statistic.service.ts
@@ -1,4 +1,51 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
+import { PrismaService } from '../prisma.service'
 
 @Injectable()
-export class StatisticService {}
+export class StatisticService {
+	constructor(private prisma: PrismaService) {}
+	async getStatistic(userId: number) {
+		const user = await this.prisma.user.findUnique({
+			where: { id: userId },
+			select: {
+				orders: {
+					select: {
+						items: true
+					}
+				},
+				reviews: true
+			}
+		})
+		const orders = await this.prisma.order.findMany({
+			orderBy: {
+				createdAt: 'desc'
+			},
+			where: { userId }
+		})
+		return [
+			{
+				name: 'Orders',
+				value: user.orders.length
+			},
+			{
+				name: 'Reviews',
+				value: user.reviews.length
+			},
+			{
+				name: 'Totals amount',
+				value: user.orders.reduce((acc, order) => {
+					return (
+						acc +
+						order.items.reduce((acc, item) => {
+							return acc + item.price
+						}, 0)
+					)
+				}, 0)
+			},
+			{
+				name: 'Last order date',
+				value: orders[0].createdAt
+			}
+		]
+	}
+}
