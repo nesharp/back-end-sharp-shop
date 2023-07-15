3adb015c940951e7dd182bd64324f50c7bed7990
Michael <nesterukmisha27@gmail.com>
2023-07-15 12:02:35 +0300

    false-changes

--- dist/main.js
+++ dist/main.js
@@ -9,7 +9,7 @@ async function bootstrap() {
     await prismaService.enableShutdownHooks(app);
     app.setGlobalPrefix('/api');
     app.enableCors();
-    await app.listen(4200);
+    await app.listen(3000);
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
