6613b006a7201b19b81b3e2dd0a5616ce1ba61e7
nesharp <nesterukmisha27@gmail.com>
2023-09-06 22:00:07 +0300

    falsechanges

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,6 @@ async function bootstrap() {
 	// app.setGlobalPrefix('/api')
 	app.enableCors()
 	// await app.listen(4200)
-	await app.listen(process.env.PORT || 3000, '0.0.0.0')
+	await app.listen(process.env.PORT || 3000)
 }
 bootstrap()
